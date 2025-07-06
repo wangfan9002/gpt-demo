fd1f707603122de369c0d6166e2954c495a6a64f
348349361 <348349361@qq.com>
2025-07-06 15:54:47 +0800

    ai

--- src/main/java/com/gy/gpt/controller/ChatController.java
+++ src/main/java/com/gy/gpt/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.gy.gpt.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.gy.gpt.api.ChatRequest;
@@ -61,16 +62,25 @@ public class ChatController {
                 Map<String, String> msgMap = new HashMap();
                 msgMap.put("role", "user");
                 msgMap.put("content", prompt);
+                dataMap.put("model", "Baichuan-M1-14B-Instruct");
                 dataMap.put("messages", Arrays.asList(msgMap));
-
+                dataMap.put("temperature", 0);
+                log.info("request:{}", JSON.toJSON(dataMap));
                 HttpEntity<Map<String, Object>> request = new HttpEntity<>(dataMap, headers);
 
                 String answer = restTemplate.postForObject(serverUrl, request, String.class);
-                // 1. 解析为 JSONObject
-                JSONObject jsonObject = JSON.parseObject(answer);
-                String response = jsonObject.getString("response");
                 log.info("answer:{}", answer);
-
+                // 1. 解析为 JSONObject
+                // {"model":"Baichuan-M1-14B-Instruct","choices":[{"index":0,"message":{"role":"assistant","content":"I'm Assistant. How can I assist you today?","tool_calls":null},"finish_reason":"stop","logprobs":null}],"usage":{"prompt_tokens":14,"completion_tokens":13,"total_tokens":27}
+                // ,"id":"chatcmpl-4b8b65409730468ab9af257a7f93143f","object":"chat.completion","created":1751210854
+                JSONObject jsonObject = JSON.parseObject(answer);
+                JSONArray choices = jsonObject.getJSONArray("choices");
+                log.info("choices:{}", choices);
+                JSONObject oneChoice = choices.getJSONObject(0);
+                log.info("oneChoice:{}", oneChoice);
+                JSONObject jsonMessage = oneChoice.getJSONObject("message");
+                log.info("jsonMessage:{}", jsonMessage);
+                String response = jsonMessage.getString("content");
                 String toPrint = (answer != null && response != null) ? response : "[无]";
                 for (int i = 0; i < toPrint.length(); i++) {
                     if (stopFlags.getOrDefault(sessionId, false)) {
